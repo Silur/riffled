64e4b220db0aac8f8f49a36f2b73c9c6487b90e3
Nixkönner <56412084+nixkoenner@users.noreply.github.com>
2020-08-31 07:20:35 +0000

    made config parsing optional
    if config file doesn't exist, use defaults

--- src/main.rs
+++ src/main.rs
@@ -13,38 +13,37 @@ use net::{Client, Server, Agent};
 
 fn main() {
     info!("reading log");
-    let conf_file = match File::open("./riffle.conf") {
-        Ok(a) => a,
-        Err(e) => panic!("error opening config file: {}", e)
-    };
-    let mut reader = BufReader::new(conf_file);
-    let mut line = String::new();
     let mut main_config = config::Config::new();
+    if let Ok(conf_file) = File::open("./riffle.conf") {
+        let mut reader = BufReader::new(conf_file);
+        let mut line = String::new();
+        loop {
+            if reader.read_line(&mut line).unwrap() == 0 { break; }
+            let split: Vec<&str> = line.split("=").collect();
+            let token = split[0].trim();
+            let value = split[1].trim();
+
+            match token {
+                "mode" => { 
+                    main_config.server_mode = value == "server";
+                },
+                "host" => {
+                    main_config.host = String::from(value);
+                },
+                "port" => {
+                    main_config.port = value.parse().unwrap();
+                },
+                "primary-server" => {
+                    main_config.primary_server = String::from(value);
+                },
+                _ => warn!("ignoring unexpected config param: {}",token)
+            }
+        }
+    };
+
     if sodiumoxide::init().is_err() {
         error!("failed to initialize crypto libraries in thread-safe mode");
-    }
-    loop {
-        if reader.read_line(&mut line).unwrap() == 0 { break; }
-        let split: Vec<&str> = line.split("=").collect();
-        let token = split[0].trim();
-        let value = split[1].trim();
-
-        match token {
-            "mode" => { 
-                main_config.server_mode = value == "server";
-            },
-            "host" => {
-                main_config.host = String::from(value);
-            },
-            "port" => {
-                main_config.port = value.parse().unwrap();
-            },
-            "primary-server" => {
-                main_config.primary_server = String::from(value);
-            },
-            _ => error!("unexpected config param")
-        }
-    }
+    }  
 
     if main_config.server_mode {
         start_server(main_config);
